d33b06295375e59b6f8462d2f3a89e97942c2e4c
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-04 08:37:14 +0200

    some changes

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  secret: String(process.env.SECRET),
+  host: String(process.env.NEXT_PUBLIC_HOST),
+};
--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,1 +1,27 @@
-import { NextApiRequest , NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+// http://localhost:3000/api/register
+
+const registerUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const checkUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (checkUser) {
+    res.status(400).json({ message: "user already exists" });
+  } else {
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        phone: phone,
+      },
+    });
+    res.status(200).json({ message: "user created successfully", newUser });
+  }
+};
+
+export default registerUser;
