67056d2794a413d837dd302d0d5803a66707dff3
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-04 09:29:30 +0200

    some changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -36,7 +36,7 @@ const Login = () => {
     console.log(loginJSON);
     if (loginJSON) {
       setCookie("token", loginJSON.token);
-      router.push("/");
+      router.push("/hub");
     } else {
       alert("error");
     }
@@ -61,7 +61,6 @@ const Login = () => {
         </Text>
       </Flex>
       <Box display={"flex"} justifyContent={"center"} mt={"70px"}>
-
         <Flex
           w={"317px"}
           h={"376px"}
@@ -97,14 +96,12 @@ const Login = () => {
           </Button>
           <Flex justifyContent={"center"}>
             <Text color={"black"}>
-
-              Don't have an account? {"   "}
+              not having an account? {"   "}
               <chakra.span color={"primary.50"}>
                 <Link href={"/signup"} className={"Link"}>
                   Sign up
                 </Link>
               </chakra.span>
-
             </Text>
           </Flex>
         </Flex>
