babce2544f2c9c14a83ef10ab889a4b4e633b6ad
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-04 09:15:37 +0200

    last changes related to endpoints and integration

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,8 +2,37 @@ import React from "react";
 import { Flex, Text, VStack, Button, Box, Input } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { config } from "../config";
+import { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
 
 const Login = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const router = useRouter();
+  // so here is the logic for the login
+
+  const handleLogin = async () => {
+    const login = await fetch(`${config.host}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        phone: phone,
+      }),
+    });
+    const loginJSON = await login.json();
+    console.log(loginJSON);
+    if (loginJSON) {
+      setCookie("token", loginJSON.token);
+      router.push("/");
+    } else {
+      alert("error");
+    }
+  };
   return (
     <>
       <Flex justifyContent={"center"} pt={"32px"} id={"features"}>
@@ -29,13 +58,26 @@ const Login = () => {
           zIndex={"9999"}
           shadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
         >
-          <input className="login-input" placeholder="Email" type={"email"} />
+          <input
+            className="login-input"
+            placeholder="Email"
+            type={"email"}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             className="login-input"
             placeholder="Phone #"
             type={"number"}
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
-          <Button variant={"ghost"} colorScheme={"green"}>
+          <Button
+            type="submit"
+            variant={"ghost"}
+            colorScheme={"green"}
+            onClick={handleLogin}
+          >
             Submit
           </Button>
           <Flex justifyContent={"center"}>
